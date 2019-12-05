5c5511ba339bd28c118b8f0e2a92f7bf96cbd350
Mark Johnston <mark.johnston9001@gmail.com>
2019-12-05 11:35:14 +0000

    Worked on v1 of code base

--- camera.py
+++ camera.py
@@ -3,33 +3,69 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
- 
-# initialize the camera and grab a reference to the raw camera capture
-cam = PiCamera()
-cam.resolution = (640, 480)
-cam.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
- 
+import threading 
+import argparse
+import imutils
 # allow the cui8iamera to warmup
 class Camera:
-    def __init__(self):
-        x = threading.Thread(target=runCamera, args=(1,))
-        x.start()
-        
+    # initialize the camera and grab a reference to the raw camera capture
+    cam = PiCamera()
+    cam.resolution = (640, 480)
+    cam.framerate = 32
+    rawCapture = PiRGBArray(cam, size=(640, 480))
+    finish = False
+    imageReading = False
+    runDone = False
+    
+    def getImage(self):
+        while True:
+            self.imageReading = True
+            while(self.runDone == False):
+                pass
+            print("Starting read image")
+            time.sleep(0.1)
+            fgbg = cv2.createBackgroundSubtractorMOG2()
+            for frame in self.cam.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
+                # grab the raw NumPy array representing the image, then initialize the timestamp
+                # and occupied/unoccupied text
+                image = frame.array
+                mask = fgbg.apply(image)
+                
+                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                    cv2.CHAIN_APPROX_SIMPLE)
+                cnts = imutils.grab_contours(cnts)
+                print("I found {} changes".format(len(cnts)))
+                for c in cnts:
+                    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+                # show the frame
+                cv2.imshow("Frame", image)
+                key = cv2.waitKey(1) & 0xFF
+                # clear the stream in preparation for the next frame
+                self.rawCapture.truncate(0)
+
+    def exit(self):
+        self.finish = True
         
     def isMovement(self):
         pass
         
     # capture frames from the camera
     def runCamera(self):
-        for frame in cam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        self.runDone = False
+        for frame in self.cam.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
             # grab the raw NumPy array representing the image, then initialize the timestamp
             # and occupied/unoccupied text
             image = frame.array
-         
-            # show the frame
-            cv2.imshow("Frame", image)
-         
+            self.imgGet = image
             # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
+            self.rawCapture.truncate(0)
+            if self.finish or self.imageReading:
+                print("Exiting camera module")
+                self.runDone = True
+                break
          
+    def __init__(self):
+        x = threading.Thread(target=self.runCamera)
+        x.start()
+        print("Camera module initalized")
+
--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import camera;
 import network;
 
 def main():
+    c = camera.Camera()
+    print("All modules initalized.")
+    while(1):
+        read = raw_input("Please enter command: ")
+        print("Read command: " + read)
+        try:
+            if read == "readimage":
+                c.getImage()
+            if read == "end":
+                c.exit()
+                print("Exiting program")
+                break
+        except:
+            print("Something went wrong")
     
-    
-    
-if __name__ == "__main__"
+if __name__ == "__main__":
    main();
-   
